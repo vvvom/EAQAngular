32de7af2bf59de775850075db07d59a1984851f1
KobetiakMykola <kobetiakmykola@ukr.net>
2019-04-04 17:35:30 +0300

    beta for logination and get payments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from './services/user.service';
+import {Response} from './models/Response';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'EAQAngular';
+export class AppComponent implements OnInit {
+
+  constructor(
+    private userService: UserService
+  ) {
+  }
+
+  userData;
+  data = [];
+
+  sendUserData(name: string, password: string): void {
+
+    this.userData = {
+      name,
+      password
+    };
+
+    this.userService.sendDataUser(this.userData)
+      .subscribe( (response: Response) => {
+          if (response.success) {
+            localStorage.setItem('token', response.message);
+          }
+      });
+  }
+
+  ngOnInit(): void {
+  }
+
+  getData() {
+      this.userService.getData()
+        .subscribe((response: Response) => {
+          if (response.success) {
+            this.data = response.message;
+          }
+        });
+  }
 }
